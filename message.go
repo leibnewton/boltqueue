58061b688ec60956c1615c875274846c704cbf56
dongzhou.xie <dongzhou.xie@qq.com>
2020-03-20 00:06:37 +0800

    add two methods on []byte for Message

--- message.go
+++ message.go
@@ -12,6 +12,10 @@ func NewMessage(value string) *Message {
 	return &Message{nil, []byte(value), -1}
 }
 
+func NewBytesMessage(value []byte) *Message {
+	return &Message{nil, value, -1}
+}
+
 // Priority returns the priority the message had in the queue in the range of
 // 0-255 or -1 if the message is new.
 func (m *Message) Priority() int {
@@ -21,4 +25,8 @@ func (m *Message) Priority() int {
 // ToString outputs the string representation of the message's value
 func (m *Message) ToString() string {
 	return string(m.value)
+}
+
+func (m *Message) Bytes() []byte {
+	return m.value
 }
